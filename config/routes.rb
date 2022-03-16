96f047edb73ed5ecb647c4d61fc55babf9619bb3
txphuc <txp.sun@gmail.com>
2022-03-16 05:42:17 -0700

    Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
     resources :books, only: %i(index show)
 
     root "static_pages#home"
+    get "/home", to: "static_pages#home"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
+
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users
   end
 end
