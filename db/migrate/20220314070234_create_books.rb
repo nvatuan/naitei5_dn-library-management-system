df8adbc99a85281b26c21d527f916f8b2d207857
nvatuan <24392632+nvatuan@users.noreply.github.com>
2022-03-17 09:26:48 +0700

    Books Show

--- db/migrate/20220314070234_create_books.rb
+++ db/migrate/20220314070234_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title
       t.string :description
-      t.integer :quantity
+      t.integer :quantity, default: 0
       t.date :published_date
       t.references :category, null: true, foreign_key: true
       t.references :publisher, null: true, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create Categories
+cate_edu = Category.create(name: "Educational")
+cate_edu.children << Category.create(name: "Science")
+cate_edu.children << Category.create(name: "Social")
+cate_edu.children << Category.create(name: "Politics")
+cate_edu.children << Category.create(name: "Finance")
 
-cate = Category.create(name: "Fictional")
-publ = Publisher.create(name: "FPubl", description: "This publisher publishes "\
-                                                    "fictional works.")
+cate_lit = Category.create(name: "Literature")
+cate_novel = Category.create(name: "Novel")
+cate_poem = Category.create(name: "Poem")
 
+cate_lit.children << cate_novel << cate_poem
+
+cate_novel.children << Category.create(name: "Fictional")
+cate_novel.children << Category.create(name: "Romance")
+cate_poem.children << Category.create(name: "Nature")
+
+cates = Category.all
+
+# Create Publishers
+5.times {
+  Publisher.create(
+    name: Faker::Company.name,
+    description: Faker::Lorem.paragraph(sentence_count: rand(4..10))
+  )
+}
+
+pubs = Publisher.all
+
+# Create Books
 42.times {
   Book.create(
     title: Faker::Hipster.sentences.sample,
-    category_id: cate.id,
-    publisher_id: publ.id,
+    category_id: cates.sample.id,
+    publisher_id: pubs.sample.id,
+    quantity: rand(0..10),
     description: Faker::Lorem.paragraph(sentence_count: rand(4..8)),
+    published_date: Faker::Date.between(from: 10.years.ago, to: 1.year.ago),
     status: 2,
   )
 }
 Book.create(
   title: "Not published",
-  category_id: cate.id,
-  publisher_id: publ.id,
+  category_id: cates.sample.id,
+  publisher_id: pubs.sample.id,
+  quantity: 0,
   description: "Not published",
+  published_date: Date.today,
   status: 0,
 )
 Book.create(
   title: "Not available",
-  category_id: cate.id,
-  publisher_id: publ.id,
+  category_id: cates.sample.id,
+  publisher_id: pubs.sample.id,
+  quantity: 0,
   description: "Not available",
+  published_date: Date.today,
   status: 1,
 )
