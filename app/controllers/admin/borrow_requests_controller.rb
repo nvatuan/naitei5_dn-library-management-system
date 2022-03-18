22e48af3e563c146eb3fb799f9ec0291f208f95d
nvatuan <24392632+nvatuan@users.noreply.github.com>
2022-03-18 11:51:57 +0700

    Admin BorrowRequests Index Search

--- app/controllers/admin/borrow_requests_controller.rb
+++ app/controllers/admin/borrow_requests_controller.rb
@@ -2,7 +2,30 @@ class Admin::BorrowRequestsController < Admin::AdminController
   before_action :check_admin_logged_in, only: %i(index show edit)
 
   def index
-    @pagy, @borrow_reqs = pagy BorrowRequest.ordered_by_status
+    keyword, sort_kw = params.values_at :keyword, :sort_by
+
+    # Check if sort keyword doesn't exists then use default instead
+    sort_kw = "created_at" if sort_kw.blank?
+    @borrow_reqs = BorrowRequest.ordered_by sort_kw
+
+    # Collect "status"="1" into array of statuses
+    # params = {
+    #   "status_pending": "0", "status_ready": "1",
+    #   "status_borrowed": "0", "status_return": "1",
+    #   "status_rejected": "1", "status_cancelled: "0"
+    # }
+    # => statuses = ["ready", "return", "rejected"]
+    statuses = []
+    %w(pending ready borrowed returned rejected cancelled).each do |stt|
+      status_name = "status_#{stt}"
+      statuses << stt if params[status_name] == "1"
+    end
+    @borrow_reqs = @borrow_reqs.filter_by_status statuses
+
+    # Seach for book title and author name
+    @borrow_reqs = @borrow_reqs.search_by keyword
+
+    @pagy, @borrow_reqs = pagy @borrow_reqs
   end
 
   def show; end
--- app/models/borrow_request.rb
+++ app/models/borrow_request.rb
@@ -15,9 +15,21 @@ class BorrowRequest < ApplicationRecord
     cancelled: 5
   }, _prefix: true
 
-  scope :by_user, ->(user_id){where(user_id: user_id)}
-  scope :by_book, ->(book_id){where(book_id: book_id)}
+  default_scope{joins :book, :user}
+
+  scope :search_by, (lambda do |word|
+    where "title LIKE :kw OR name LIKE :kw", kw: "%#{word}%" if word.present?
+  end)
+
+  scope :filter_by_status, ->(stts){where(status: stts) if stts.present?}
+
+  scope :by_user, ->(user_id){where user_id: user_id}
+  scope :by_book, ->(book_id){where book_id: book_id}
+
+  scope :ordered_by_created_at, ->{order :created_at}
   scope :ordered_by_status, ->{order :status}
   scope :ordered_by_borrowed_date, ->{order :borrowed_date}
   scope :ordered_by_return_date, ->{order :return_date}
+
+  scope :ordered_by, ->(sort_kw){send("ordered_by_#{sort_kw}")}
 end
